c94fe2b1a9f0d23c02ca984e25e3c01bdf1974cc
Jack <ainiyiwanxxl@gmail.com>
2017-09-28 10:32:56 +0800

    Initial commit

---
+++ zhy_ScaleGestureDetector04/src/main/java/com/zhy/zhy_scalegesturedetector02/MainActivity.java
@@ -0,0 +1,15 @@
+package com.zhy.zhy_scalegesturedetector02;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+}
